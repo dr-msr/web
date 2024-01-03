90f0deb7fe2e282cea56a4684d46a4f2a9151b63
drmsr <me@drmsr.dev>
2024-01-04 05:24:27 +0800

    fix

--- components/mainCard.tsx
+++ components/mainCard.tsx
@@ -33,10 +33,9 @@ function RenderDay() {
 				<br></br>
 				<p><span style={{color:'#2e95d3'}}>const</span> executeRoutine = {'('}<span style={{color:'#df3079'}}>routine</span>: <span style={{color:'#f22c3d'}}>DailyRoutine</span>{')'}: void ={'>'} {'{'}</p>
   				<span className="p-10"><span style={{color:'#2e95d3'}}>if</span> (routine === <span style={{color:'#00a67d'}}>'day'</span>) {'{'} </span>
-   				<span className="p-20"><span style={{color:'#df3079'}}>write</span>.prescriptions();</span>
-				<p>
+   				<p><span className="p-20"><span style={{color:'#df3079'}}>write</span>.prescriptions();</span></p>
 				<span className="p-10">{'}'} <span style={{color:'#2e95d3'}}>else if</span> (routine === <span style={{color:'#00a67d'}}>'night'</span>) {'{'} </span>
-				<span className="p-20"><span style={{color:'#df3079'}}>write</span>.codes(); </span></p>
+				<p><span className="p-20"><span style={{color:'#df3079'}}>write</span>.codes(); </span></p>
 				<span className="p-10"> {'}'} </span>
 				<p>
 				{'}'}; </p>
