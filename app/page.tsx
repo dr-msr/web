b5c022bd5d1f2e4ff376c4ec0d346f31c880c37d
drmsr <me@drmsr.dev>
2024-01-04 17:18:54 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
 	<div className='bg-white dark:bg-black flex justify-center content-center flex-row h-full w-full'>
-  <MainCard/>
+		<div className='max-w-md'>
+		<MainCard/>
+
+
+		</div>
   </div>
   )
 }
