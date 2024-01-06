7d1a88c88fe3f2978ae1bc5b884bec2ba53fbcea
drmsr <me@drmsr.dev>
2024-01-07 03:32:33 +0800

    fix useEffect missing deps

--- app/container.tsx
+++ app/container.tsx
@@ -20,6 +20,7 @@ export default function Container() {
 		  document.removeEventListener('keydown', leftright);
 		};
 
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 		}, []);
 	
 	return (
