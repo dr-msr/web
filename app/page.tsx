85164d856c4287d693bf2f9cc6c36eade70ec5e3
drmsr <me@drmsr.dev>
2024-01-10 03:04:07 +0800

    starfield!

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Navi } from '@/lib/navi';
 import { HnProvider  } from '@/lib/hashnode';
 import Container from './container';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Starfield from 'react-starfield';
 
 export default function Home() {
 
@@ -14,13 +15,18 @@ const client = new ApolloClient({
   })
 
 return (
-	<ApolloProvider client={client}>
-	<HnProvider>
-	<Anim8>
-		<Navi>
-			<Container />
-		</Navi>
-	</Anim8>
-	</HnProvider>
-	</ApolloProvider>
+	<><ApolloProvider client={client}>
+		<HnProvider>
+			<Anim8>
+				<Navi>
+					<Container />
+				</Navi>
+			</Anim8>
+		</HnProvider>
+	</ApolloProvider><Starfield
+			starCount={1000}
+			starColor={[255, 255, 255]}
+			speedFactor={0.05}
+			backgroundColor="black" /></>
+	
 )}
